9210fbfb1b614e4b33f2f199887ab62dd1519504
pixelr123 <atul.prasad.333@gmail.com>
2019-09-11 12:27:59 +0530

    updated heroku url

--- curl-script-generate-jwt-key.sh
+++ curl-script-generate-jwt-key.sh
@@ -1,1 +1,1 @@
-curl -X POST http://localhost:8080/auth -d '{"username": "atul", "password":"password"}' -H 'Content-type: application/json'
+curl -X POST https://bank-searches.herokuapp.com/auth -d '{"username": "atul", "password":"password"}' -H 'Content-type: application/json'
